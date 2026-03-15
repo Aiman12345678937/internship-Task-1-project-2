f57bd9fb3f6503c47ca443dcdeedc869b0503936
Aiman12345678937 <jamaliaiman64@gmail.com>
2026-03-15 19:56:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+function addTask(){
+
+let input = document.getElementById("taskInput");
+let task = input.value;
+
+if(task === "") return;
+
+let li = document.createElement("li");
+li.textContent = task;
+
+li.onclick = function(){
+this.style.textDecoration = "line-through";
+}
+
+document.getElementById("taskList").appendChild(li);
+
+input.value="";
+}
